3c5ace668a466153dab128c86b822988e47fc0e1
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-07-06 18:45:54 -0300

    creating the calculator form and adding the ngx-bootstrap to the project

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { SeriesListComponent } from './series-list/series-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { RouterModule } from '@angular/router';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [SeriesListComponent, CalculatorComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,    
+    FormsModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule
   ],
   exports: [SeriesListComponent]
 })
