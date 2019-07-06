e0b872abcff77f420ea8a1d88a6a311293f5403d
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-07-06 15:40:51 -0300

    Including queryParams into the Series Service

--- src/app/series/series.service.ts
+++ src/app/series/series.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Serie } from '../models/serie';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { take } from 'rxjs/operators';
-import { Season } from '../models/season';
+import { Serie, Season, Episode } from '../core';
+
+const mockDate = '';
+const params = new HttpParams().append('mockDate', '2019-03-01');
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,22 +17,22 @@ export class SeriesService {
   constructor(private http: HttpClient) { }
 
   listSeries(){
-    return this.http.get<Serie[]>(`${this.API}/series`)
+    return this.http.get<Serie[]>(`${this.API}/series`, {params})
     .pipe(take(1));
   }
 
   getSerie(idSerie: string){    
-    return this.http.get<Serie>(`${this.API}/series/${idSerie}`)
+    return this.http.get<Serie>(`${this.API}/series/${idSerie}`, {params})
     .pipe(take(1));
   }
 
   getSeasons(idSerie: string){
-    return this.http.get<Season>(`${this.API}/series/${idSerie}/seasons`)
+    return this.http.get<Season[]>(`${this.API}/series/${idSerie}/seasons`,{params})
     .pipe(take(1));
   }
 
   getEpisodes(idSerie:string, idSeason: string){
-    return this.http.get<Season>(`${this.API}/series/${idSerie}/seasons/${idSeason}/episodes`)
+    return this.http.get<Episode[]>(`${this.API}/series/${idSerie}/seasons/${idSeason}/episodes`, {params})
     .pipe(take(1));
   }
 }
